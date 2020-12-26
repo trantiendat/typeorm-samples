b68d645eebb9f6579356251dbd5044840f440520
Tiến Đạt Trần <trantiendat016@gmail.com>
2020-12-27 00:16:10 +0700

    Add photo data then get it from database

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,17 @@ createConnection({
   synchronize: true,
   logging: false,
 })
-  .then(async (connection) => {})
+  .then(async (connection) => {
+    let photo = new Photo();
+    photo.name = "Me and Bears";
+    photo.description = "I am near polar bears";
+    photo.fileName = "photo-with-bears.jpg";
+    photo.views = 1;
+    photo.isPublished = true;
+
+    await connection.manager.save(photo);
+
+    let savedPhotos = await connection.manager.find(Photo);
+    console.log("All photos from the db: ", savedPhotos);
+  })
   .catch((error) => console.log(error));
