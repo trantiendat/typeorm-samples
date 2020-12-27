e86f5b1e9446588a431c64075ac21bfc51864f46
Tiến Đạt Trần <trantiendat016@gmail.com>
2020-12-27 23:57:09 +0700

    Using query builder

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,18 @@ import { Album } from "./entity/Album";
 
 createConnection()
   .then(async (connection) => {
-    let album1 = new Album();
-    album1.name = "Bears";
-
-    await connection.manager.save(album1);
-
-    let album2 = new Album();
-    album2.name = "Me";
-    await connection.manager.save(album2);
-
-    let photo = new Photo();
-    photo.name = "Me and Bears";
-    photo.description = "I am near polar bears";
-    photo.fileName = "photo-with-bears.jpg";
-    photo.views = 1;
-    photo.isPublished = true;
-    photo.albums = [album1, album2];
-    await connection.manager.save(photo);
-
-    const loadedPhoto = await connection
+    let photos = await connection
       .getRepository(Photo)
-      .findOne(photo.id, { relations: ["albums"] });
-
-    console.log(loadedPhoto);
+      .createQueryBuilder("photo")
+      .innerJoinAndSelect("photo.metadata", "metadata")
+      .leftJoinAndSelect("photo.albums", "album")
+      .where("photo.isPublished = true")
+      .andWhere("(photo.name = :photoName OR photo.name = :bearName)")
+      .orderBy("photo.id", "DESC")
+      .skip(5)
+      .take(10)
+      .setParameters({ photoName: "Me and Bears", bearName: "Mishka" })
+      .getMany();
+    console.log(photos);
   })
   .catch((error) => console.log(error));
