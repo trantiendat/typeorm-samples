e009e854474411e7b6c0ceb0874977ebcd6999e9
Tiến Đạt Trần <trantiendat016@gmail.com>
2020-12-27 14:10:34 +0700

    Add new entity with one to one relationship

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,11 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { User } from "./entity/User";
 import { Photo } from "./entity/Photo";
+import { PhotoMetadata } from "./entity/PhotoMetadata";
 
-createConnection({
-  type: "postgres",
-  host: "localhost",
-  username: "root",
-  password: "",
-  database: "test",
-  entities: [Photo],
-  synchronize: true,
-  logging: false,
-})
+createConnection()
   .then(async (connection) => {
+    // create a photo
     let photo = new Photo();
     photo.name = "Me and Bears";
     photo.description = "I am near polar bears";
@@ -21,11 +14,28 @@ createConnection({
     photo.views = 1;
     photo.isPublished = true;
 
-    await connection.manager.save(photo);
+    // create a photo metadata
+    let metadata = new PhotoMetadata();
+    metadata.height = 640;
+    metadata.width = 480;
+    metadata.compressed = true;
+    metadata.comment = "cybershoot";
+    metadata.orientation = "portrait";
+    metadata.photo = photo; // this way we connect them
 
+    // get entity repositories
     let photoRepository = connection.getRepository(Photo);
+    let metadataRepository = connection.getRepository(PhotoMetadata);
 
-    let photoToRemove = await photoRepository.findOne(1);
-    await photoRepository.remove(photoToRemove);
+    // first we should save a photo
+    await photoRepository.save(photo);
+
+    // photo is saved. Now we need to save a photo metadata
+    await metadataRepository.save(metadata);
+
+    // done
+    console.log(
+      "Metadata is saved, and relation between metadata and photo is created in the database too"
+    );
   })
   .catch((error) => console.log(error));
